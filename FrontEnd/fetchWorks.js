e3e22b981401342dc95398563cf9d9b8a6897bf7
Medina Kone <kone-medina@hotmail.com>
2024-05-01 16:09:55 +0200

    feat : création d'un premier bouton de filtres

--- FrontEnd/fetchWorks.js
+++ FrontEnd/fetchWorks.js
@@ -1,6 +1,34 @@
-const worksEndpoint = 'http://localhost:5678/api/works';
+const worksEndpoint = 'http://localhost:5678/api/';
 
-fetch(worksEndpoint)
+function category() {
+    const sectionGallery = document.querySelector(".gallery");
+    const sectionPortfolio = document.querySelector("#portfolio");
+     let divButton = document.createElement("div")
+    divButton.setAttribute("class","filtres")
+
+    let Button = document.createElement("button")
+    Button.setAttribute("class","button")
+    Button.innerText="Tous"
+
+    divButton.append(Button)
+
+    
+   sectionPortfolio.insertBefore(divButton,sectionGallery)
+} 
+
+async function afficheButton() {
+    let response = await fetch(worksEndpoint+"categories")
+    let categories = await response.json()
+   
+        for(let i = 0; i < categories.length; i++){
+            console.log(categories[i].name)
+category()
+}}
+afficheButton()
+
+
+/**
+fetch(worksEndpoint +works)
     .then((response) => {
         if (!response.ok) {
             throw new Error(`Erreur: ${response.status} - ${response.statusText}`);
@@ -10,7 +38,13 @@ fetch(worksEndpoint)
     .then((data) => {
         console.log('Données récupérées:', data);
 
-/**Création des éléments */
+
+// Création des sous-éléments de l'élément "catégorie" 
+const nameElement = document.createElement("p");
+nameElement = "name";
+
+
+//Création des éléments 
         const imageElement = document.createElement("img");
         imageElement.src = "http://localhost:5678/images/abajour-tahina1651286843956.png";
 
@@ -29,17 +63,11 @@ fetch(worksEndpoint)
         const categoryElement = document.createElement("p");
         categoryElement = "category";
 
-/** Création des sous-éléments de l'élément "catégorie" */
-        const idElement = document.createElement("p");
-        idElement.innerText  = 'id';
 
-        const nameElement = document.createElement("p");
-        nameElement = "name";
-
-/**Récupération de la section Gallery */
+//Récupération de la section Gallery 
         const sectionGallery = document.querySelector(".gallery");
 
-/**Ajout des éléments à la section gallery */
+//Ajout des éléments à la section gallery
         sectionGallery.appendChild(imageElement);
         sectionGallery.appendChild(titleElement);
         sectionGallery.appendChild(idElement);
@@ -52,5 +80,5 @@ fetch(worksEndpoint)
     .catch((error) => {
         console.error('Erreur lors de la récupération des fichiers:', error);
     });
-
+*/
 
